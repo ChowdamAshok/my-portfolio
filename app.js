4166de975c19c4e3ac29883f4b8f7e9b044b82b0
ChowdamAshok <161301092+ChowdamAshok@users.noreply.github.com>
2024-05-08 16:10:06 +0530

    app.js

--- app.js
+++ app.js
@@ -11,19 +11,7 @@ function yourCode() {
   window.onscroll = () => {
       navbar.classList.remove('active');
   }
-  // Dark Mode
-  let darkmode = document.querySelector('#darkmode');
-
-  darkmode.onclick = () => {
-      if(darkmode.classList.contains('bx-moon')){
-          darkmode.classList.replace('bx-moon','bx-sun');
-          document.body.classList.add('active');
-      }else{
-          darkmode.classList.replace('bx-sun','bx-moon');
-          document.body.classList.remove('active');
-      }
-  }
-
+ 
   // Scroll Reveal
   const sr = ScrollReveal ({
       origin: 'left',
